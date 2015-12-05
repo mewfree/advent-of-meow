106aa2bf0f2dff359b592925432bc27ab1606216
mewfree <damien.gonot@gmail.com>
2015-12-05 17:57:56 -0500

    Refactoring Lua code

--- day-04/day-04.lua
+++ day-04/day-04.lua
@@ -1,24 +1,12 @@
 local md5 = require 'md5'
 input = 'yzbqklnj'
 
-print('Part 1:')
 local i = 1
+local done1 = false
+
 while true do
   hash = md5.sumhexa(input .. i)
-  if string.sub(hash, 1, 5) == '00000' then
-    print(i)
-    break
-  end
+  if string.sub(hash, 1, 5) == '00000' and not done1 then done1 = true print('Part 1: ' .. i) end
+  if string.sub(hash, 1, 6) == '000000' then print('Part 2: ' .. i) break end
   i = i + 1
-end
-
-print('Part 2:')
-local j = 1
-while true do
-  hash = md5.sumhexa(input .. j)
-  if string.sub(hash, 1, 6) == '000000' then
-    print(j)
-    break
-  end
-  j = j + 1
 end
